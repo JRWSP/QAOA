0da678a883bd567cf2ed4c01edbb4638e9087349
Jirawat S <57433713+JRWSP@users.noreply.github.com>
2020-08-18 16:55:23 +0700

    Add files via upload

--- Parallel_test.py
+++ Parallel_test.py
@@ -34,7 +34,7 @@ def cost_function_C(x,G):
         e1 = index[0]
         e2 = index[1]
         w      = G[e1][e2]['weight']
-        #t_w = -1.0*(G[e1][e2]['weight']**2)/(2.0* sigma**2)
+        #t_w = 1.0*(G[e1][e2]['weight']**2)/(2.0* sigma**2)
         #w = np.e**t_w
         C = C + w*x[e1]*(1-x[e2]) + w*x[e2]*(1-x[e1])
     return C
@@ -51,9 +51,9 @@ def Qiskit_QAOA(beta, gamma, V, E):
         for edge in E:
             k = edge[0]
             l = edge[1]
-            w = gamma[a]*G[k][l]['weight']
-            #t_w = -1.0*(G[k][l]['weight']**2)/(2.0* sigma**2)
-            #w = gamma[a]* np.e**t_w
+            #w = gamma[a]*G[k][l]['weight']
+            t_w = 1.0*(G[k][l]['weight']**2)/(2.0* sigma**2)
+            w = gamma[a]* np.e**t_w
             """
             QAOA.cx(k, l)
             QAOA.rz(w, l)
@@ -106,87 +106,59 @@ def optim(params):
 if __name__ == "__main__":
     #N = 8
     #Create graph from adjacency matrix
-    sigma = 1.0
-    for n in tqdm([6, 10]):
-        #data = np.load('./Demo/w'+str(deg)+'R_8n.npy', allow_pickle=True)
-        data = np.load('./wC/'+str(n)+"nodes_10samples.npy", allow_pickle=True)
-        dist = data[0]['dist']
+    for sigma in [1.0]:
         
-        G = nx.from_numpy_matrix(dist)
-        n = len(G.nodes())
-        V = np.arange(0, n, 1)
-        E = []
-        for e in G.edges():
-            E.append((e[0], e[1], G[e[0]][e[1]]['weight']))
-            #E.append((e[0], e[1], dist[e[0]][e[1]]))
-        
-        n_cores = 1
-        #Iters = 48
-        for p in range(1,2):
+        for n in tqdm([6]):
+            #data = np.load('./Demo/w'+str(deg)+'R_8n.npy', allow_pickle=True)
+            data = np.load('./wC/'+str(n)+"nodes_10samples.npy", allow_pickle=True)
+            dist = data[0]['dist']
             
-            #Construct initial boundaries as constraints
-            bnds = {'beta': 0.50*np.pi, 'gamma': 1.00*np.pi}
-            bounds = [ [ -bnds['beta'], bnds['beta'] ], 
-                       [ -bnds['gamma'], bnds['gamma'] ] ]
-            cons = []
-            for factor in range(len(bounds)):
-                lower, upper = bounds[factor]
-                l = {'type': 'ineq',
-                     'fun': lambda x, lb=lower, i=factor: x[i] - lb}
-                u = {'type': 'ineq',
-                     'fun': lambda x, ub=upper, i=factor: ub - x[i]}
-                for ii in range(p):
-                    cons.append(l)
-                    cons.append(u)
-
-            #Prepare Qiskit framework
-            backend      = Aer.get_backend("qasm_simulator")
-            shots        = 2**(n+2)
+            G = nx.from_numpy_matrix(dist)
+            n = len(G.nodes())
+            V = np.arange(0, n, 1)
+            E = []
+            for e in G.edges():
+                E.append((e[0], e[1], G[e[0]][e[1]]['weight']))
+                #E.append((e[0], e[1], dist[e[0]][e[1]]))
             
-            #Sinigle core calculation
-            Result = []
-            for bbeta in np.linspace(-0.5, 0.5, 101):
-                bbeta = np.round(bbeta, 3)
-                print("\n n"+str(n)+"beta"+str(bbeta))
-                for ggamma in np.linspace(-1.0, 1.0, 201):
-                    ggamma - np.round(ggamma, 3)
-                    init_params = list(np.concatenate(( [bbeta] , [ggamma])))
-                    counts = Qiskit_QAOA( init_params[:p], init_params[p:], V, E)
-                    avr_C       = 0
-                    for sample in list(counts.keys()):
-                        # use sampled bit string x to compute C(x)
-                        x         = [int(num) for num in list(sample)]
-                        tmp_eng   = cost_function_C(x,G)
-                        # compute the expectation value and energy distribution
-                        avr_C     = avr_C    + counts[sample]*tmp_eng
-                    M1_sampled   = -avr_C/shots
-                    temp_res = {"beta": bbeta,"gamma": ggamma, "cost": np.round(M1_sampled, 3)}
-                    Result.append(temp_res)
+            n_cores = 24
+            Iters = 24
+            for p in range(1,11):
+                #Construct boundaries as constraints
+                bnds = {'beta': 0.25*np.pi, 'gamma': 0.25*np.pi}
+                bounds = [ [ -bnds['beta'], bnds['beta'] ], 
+                           [ -bnds['gamma'], bnds['gamma'] ] ]
+                cons = []
+                for factor in range(len(bounds)):
+                    lower, upper = bounds[factor]
+                    l = {'type': 'ineq',
+                         'fun': lambda x, lb=lower, i=factor: x[i] - lb}
+                    u = {'type': 'ineq',
+                         'fun': lambda x, ub=upper, i=factor: ub - x[i]}
+                    for ii in range(p):
+                        cons.append(l)
+                        cons.append(u)
+    
+                backend      = Aer.get_backend("qasm_simulator")
+                shots        = 2**(n+2)
+                
+                Result = []
+                history = np.zeros(2*p)
+                def task(x):
+                    global history
+                    #Random initial parameters
+                    init_params = list(np.concatenate(( rand_params('beta', bnds, p) , rand_params('gamma', bnds, p))))
+                    if x%4 ==0:
+                        print("\n sigma = "+str(sigma)+" n = " + str(n) + "p = " + str(p) + " Iteration " + str(x) + "/" + str(Iters-1) )
+                    res = optimize.minimize(optim, init_params, method='Powell', tol=1e-2)
+                    if res.success:
+                        return (res, history)
+                    else:
+                        raise ValueError(res.message)
             
-            #init_params = list(np.concatenate(( rand_params('beta', bnds, p) , rand_params('gamma', bnds, p))))
-            
-            #Parallel Optimization
-            """
-            Result = []
-            history = np.zeros(2*p)
-            def task(x):
-                global history
-                #Random initial parameters
-                init_params = list(np.concatenate(( rand_params('beta', bnds, p) , rand_params('gamma', bnds, p))))
-                if x%4 ==0:
-                    print("\n n = " + str(n) + "p = " + str(p) + " Iteration " + str(x) + "/" + str(Iters-1) )
-                res = optimize.minimize(optim, init_params, method='Powell', tol=1e-2)
-                if res.success:
-                    return (res, history)
-                else:
-                    raise ValueError(res.message)
-                    
-            with Pool(n_cores) as P:
-                Sub_sample = P.map(task, range(Iters))
-            Result.append(Sub_sample)
-            #filename = "./QAOA_Data/var2.0/p" +str(p) +"shots" +str(shots)
-            #filename = "./wC/wC_gaussian"+str(n)+"nodes_p" +str(p)
-            """
-            
-            filename = "./grid/grid"+str(n)+"nodes_p" +str(p)
-            np.save(filename, Result, allow_pickle=True)
+                with Pool(n_cores) as P:
+                    Sub_sample = P.map(task, range(Iters))
+                Result.append(Sub_sample)
+                #filename = "./QAOA_Data/var2.0/p" +str(p) +"shots" +str(shots)
+                filename = "./grid/grid_N"+str(n)+"_p"+str(p)+"_specific_initiial"
+                np.save(filename, Result, allow_pickle=True)
