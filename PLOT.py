0b9a7bc3526954e373d3904d70e756ef48e19820
Jirawat S <57433713+JRWSP@users.noreply.github.com>
2020-11-12 15:42:56 +0700

    Add files via upload

--- PLOT.py
+++ PLOT.py
@@ -7,7 +7,8 @@ Created on Sat Apr 25 16:20:46 2020
 
 import numpy as np
 import matplotlib.pyplot as plt
-from config import FILENAME, smple, n
+from matplotlib import cm
+from config import FIGNAME, FILE_RI, FILE_HEURISTIC, smple, n
 
 sol6 = [-52.419, -35.872, -56.679, -51.35, -45.872, -55.649, -52.52, -57.38, -50.07, -51.043]
 sol6_10dis = [-90.277, -89.716]
@@ -45,30 +46,86 @@ low_err = 100*(mean - low_err)
 plt.errorbar(range(1, 11), 100*mean, yerr=[low_err, upp_err], color="C0", fmt='o')
 """
 
+last = 11
 #for smple in range(1, 2):
+Min_heu = []
+mean_heu = []
+std_heu = []
+
 Min6 = []
-for p in range(1,11):
-    temp = np.array([])
-    filename = FILENAME(n, p, smple)
-    data_temp = np.load(filename+".npy", allow_pickle=True)
+mean = []
+std = []
+for p in range(1,last):
+    mean_temp = 0
+    std_temp = 0
+
+    sol_min = sol6_10dis[smple]
+    """
+    file_heu = FILE_HEURISTIC(n, p, smple)
+    data_temp = np.load(file_heu+".npy", allow_pickle=True)
     #data_temp = data_temp[::2]
     temp = np.array([data_temp[0][ii][0].fun for ii in range(len(data_temp[0]))])
-    """
+
     mean_temp = np.mean(temp)
     std_temp = np.std(temp)
-    mean = np.append(mean, mean_temp/sol6[smple])
-    std = np.append(std, std_temp/sol6[smple])
+    mean_heu = np.append(mean_heu, mean_temp/sol_min)
+    std_heu = np.append(std_heu, std_temp/sol_min)
+    Min_heu.append(np.min(temp)/sol_min)#sol10[smple])
     """
-    print(np.min(temp))
-    Min6.append(np.min(temp)/sol6[smple])#sol10[smple])
-plt.scatter(range(1, 11), Min6)
-plt.ylabel("Approx. ratio (%)")
-#plt.xticks([])
-plt.title("Heuristic, 240initials")
-#plt.legend(["p=1", "p=3", "p=5"])
-#plt.savefig("./fig/heuristic", dpi=250)
+
+    file = FILE_HEURISTIC(n, p, smple)
+    data_temp = np.load(file+".npy", allow_pickle=True)
+    #data_temp = data_temp[::2]
+    temp = np.array([data_temp[0][ii][0].fun for ii in range(len(data_temp[0]))])
+
+    Op_Idx = np.argmin(temp)
+    Op_fun = np.min(temp)
+
+    mean_temp = np.mean(temp)
+    std_temp = np.std(temp)
+    mean = np.append(mean, mean_temp/sol_min)
+    std = np.append(std, std_temp/sol_min)
+    Min6.append(Op_fun/sol_min)#sol10[smple])
+
+#Scatter avg and best costs with std error.
+"""
+plt.errorbar(np.arange(1, 11)+0.1, mean_heu,
+             std_heu,
+             fmt='o',
+             capsize=5,
+             color='b',
+             ecolor='b',
+             markerfacecolor='None',
+             label="Heuristic search")
+plt.scatter(np.arange(1, 11)+0.1, Min_heu,
+            marker='x',
+            color='b')
+"""
+plt.errorbar(np.arange(1, last), mean,
+             std,
+             fmt='o',
+             capsize=5,
+             color='k',
+             ecolor='k',
+             markerfacecolor='None',
+             label="Heuristic search")
+plt.scatter(np.arange(1, last), Min6,
+            marker='x',
+            color='k')
+plt.ylabel("Approx. ratio")
+plt.ylim([0.60, 1.0])
+plt.xlabel("p")
+plt.xticks(range(1,11))
+#plt.title("Heuristic, 240initials")
+plt.legend()
+plt.grid(alpha=0.5)
+#plt.savefig(FIGNAME(n, smple), dpi=250)
 plt.show()
 
+
+
+
+#Comparing two heuristic strategies
 """
 #arr_max = [0.772, 0.837, 0.845, 0.851, 0.852]
 #Heu_max = [0.777, 0.902, 0.923, 0.941, 0.954]
@@ -81,18 +138,4 @@ plt.xlabel("p")
 plt.ylabel("Approx. ratio (%)")
 plt.legend(["fix prior params", "optimize all params"])
 plt.savefig("./fig/maxR", dpi=150)
-"""
-
-
-"""
-wC_mean = -np.mean(wC["nodes"+str(n)], axis=1)/(max_wC[n])
-upp_err = [-np.min(a)/max_wC[n] for a in wC['nodes'+str(n)]]
-upp_err = 100*(upp_err - wC_mean)
-low_err = [-np.max(a)/max_wC[n] for a in wC['nodes'+str(n)]]
-low_err = 100*(wC_mean - low_err)
-plt.errorbar(range(1, 11), 100*wC_mean, yerr=[low_err, upp_err], color="C0", fmt='x')
-plt.ylabel("r (%)")
-plt.xlabel("p")
-plt.legend()
-plt.xticks(np.arange(1, 11))
 """
--- Parallel_main.py
+++ Parallel_main.py
@@ -9,7 +9,7 @@ import numpy as np
 #import scipy.optimize as optimize
 #import networkx as nx
 from multiprocessing import Pool
-from tqdm import tqdm, trange
+from tqdm import tqdm
 import task
 from config import *
 
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     #n = 6
     n_cores = 24
     Iters = 240
-    for p in range(1, 11):
+    for p in range(10, 11):
         print("\n p: ", p)
         Result = []
         if p == 1:
@@ -25,10 +25,7 @@ if __name__ == "__main__":
                 Sub_sample = list(tqdm(P.imap(task.task_init, range(Iters)), total=Iters))
             Result.append(Sub_sample)
         elif p > 1:
-            #filename = "./Heuristic/Heuristic_N"+str(n)+"_p"+str(p-1)+"_sample" +str(smple)
-            #filename = "./Heuristic/Heuristic_N"+str(n)+"_p"+str(p-1)+"_sample" +str(smple) + "10dis"
-            filename = FILENAME(n, p-1, smple)
-            data_temp = np.load(filename+".npy", allow_pickle=True)
+            data_temp = np.load(FILE_HEURISTIC(n, p-1, smple)+".npy", allow_pickle=True)
             params = np.array([data_temp[0][ii][0].x for ii in range(len(data_temp[0]))])
             #params = data_temp[0][np.argmin(temp)][0].x
             params = np.insert(params, p-1, [params[idx, p-2] for idx in range(len(params))], axis=1)
@@ -43,6 +40,5 @@ if __name__ == "__main__":
             raise TypeError("Check func")
 
 
-        #filename = "./Heuristic/Heuristic_N"+str(n)+"_p"+str(p)+"_sample" + str(smple) +"10dis_norm"
-        filename = FILENAME(n, p, smple)
+        filename = FILE_HEURISTIC(n, p, smple)
         np.save(filename, Result, allow_pickle=True)
